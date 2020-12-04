77d713703f910816493a5007110009a4b9859894
ESC <youhavethewrong@gmail.com>
2020-12-04 13:36:20 -0600

    Refactor to reuse a bit more, also preserve prior main content as fns.

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,7 @@ use aoc_2020::day3::{place_trees, trees_encountered};
 
 fn main() -> Result<(), Error> {
     let input = std::env::args().nth(1).expect("\nprovide a filename\n");
-    let map = convert_file_to_string(&input)?;
-    let (trees, width, height) = place_trees(&map);
-    let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-    let mut tree_count: u64 = 1;
-    for slope in slopes {
-        let count = trees_encountered(&trees, width, height, slope.0, slope.1);
-        println!("Encountered {} trees.", count);
-        tree_count *= count as u64;
-    }
-    println!("{} total", tree_count);
-    Ok(())
+    day3_part_b(&input)
 }
 
 fn convert_file_to_string(input: &str) -> Result<String, Error> {
@@ -33,10 +23,7 @@ fn convert_file_to_string(input: &str) -> Result<String, Error> {
 
 #[allow(dead_code)]
 fn convert_file_contents_to_numbers(input: &str) -> Result<Vec<u32>, Error> {
-    let file = File::open(input)?;
-    let mut buf_reader = BufReader::new(file);
-    let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents)?;
+    let contents = convert_file_to_string(input)?;
     let lines = contents.split("\n");
     let mut numbers: Vec<u32> = vec![];
     for line in lines {
@@ -50,10 +37,7 @@ fn convert_file_contents_to_numbers(input: &str) -> Result<Vec<u32>, Error> {
 
 #[allow(dead_code)]
 fn convert_file_contents_to_passwords(input: &str) -> Result<Vec<Password>, Error> {
-    let file = File::open(input)?;
-    let mut buf_reader = BufReader::new(file);
-    let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents)?;
+    let contents = convert_file_to_string(input)?;
     let lines = contents.split("\n");
     let mut passwords: Vec<Password> = vec![];
     for line in lines {
@@ -63,4 +47,18 @@ fn convert_file_contents_to_passwords(input: &str) -> Result<Vec<Password>, Erro
         }
     }
     Ok(passwords)
+}
+
+fn day3_part_b(input: &str) -> Result<(), Error> {
+    let map = convert_file_to_string(&input)?;
+    let (trees, width, height) = place_trees(&map);
+    let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let mut tree_count: u64 = 1;
+    for slope in slopes {
+        let count = trees_encountered(&trees, width, height, slope.0, slope.1);
+        println!("Encountered {} trees.", count);
+        tree_count *= count as u64;
+    }
+    println!("{} total", tree_count);
+    Ok(())
 }
